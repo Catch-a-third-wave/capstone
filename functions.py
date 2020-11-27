5cfd4404d809596323e2a3bb858f7bc1f74e568e
Laura Dell'Antonio <71877418+lauradellantonio@users.noreply.github.com>
2020-11-27 13:26:30 +0100

    Updating working branch EDA Social Distancing (#22)
    * Added a function that deals with NaNs and one to create a df.
    
    * Notebook with US state groupings
    
    * Eddited country groups, created init file and get_group python file
    
    * Added package to environment and text to readme and made HDI import function. (#19)
    
    Co-authored-by: Jakolien den Hollander <jakoliendenhollander@Jakoliens-MBP.fritz.box>
    
    * Hdi country grouping (#21)
    
    * Added package to environment and text to readme and made HDI import function.
    
    * Added the functions for the HDI data.
    
    Co-authored-by: Jakolien den Hollander <jakoliendenhollander@Jakoliens-MBP.fritz.box>
    
    Co-authored-by: Jakolien den Hollander <72009367+jakoliendenhollander@users.noreply.github.com>
    Co-authored-by: Jakolien den Hollander <jakoliendenhollander@Jakoliens-MBP.fritz.box>

--- functions.py
+++ functions.py
@@ -1,10 +1,11 @@
-# Functions file for the capstone project "Cath a third wave"
+# Functions file for the capstone project "Cath a third wave".
 import pandas as pd
 import numpy as np
 import os 
 
-# Function to import multiple files into a dictionary - use for global country and region data
+# Function to import multiple files into a dictionary - use for global country and region data.
 def get_data(path, name):
+    '''Function to read in data files from csv and import it into a dictionary of dataframes - used for global country and region data.'''
     filenames = os.listdir(path)
     filenames = [f for f in filenames if f.lower().endswith(".csv")]
     filenames.sort()
@@ -17,13 +18,120 @@ def get_data(path, name):
         df_names.append(df_name)
         file = "{}/{}".format(path,filename)
         dataframes[df_name] = pd.read_csv(file)
-    
+
+    print('Read in data completed.')
     return dataframes
 
 
-# Function to make the date column the proper data type and add a month column
+# Function to make the date column the proper data type and add a month column.
 def insert_month(df):
+    '''Changes object date to datetime object date and creates a column with month.'''
     df['date'] = pd.to_datetime(df.loc[:,'date'])
     df["month"]=pd.DatetimeIndex(df['date']).month
+    print('Created month column.')
     return df
 
+# Function to update NaNs in the smoothed_pct_wear_mask_all_time_weighted with numbers from the  smoothedpct column and to remove lines of NaNs in smoothed_pct_cli.
+def deal_with_NaNs_masks(df):
+    '''Updates NaN's in smoothed_pct_wear_mask_all_time_weighted with numbers from smoothedpct_wear_mask_all_time_weighted and removes lines with NaN's in smoothed_pct_cli.'''
+    # Updating NaN's in the smoothed_pct column with numbers from the smoothedpct column.
+    df_to_update = df[['smoothedpct_wear_mask_all_time_weighted','smoothed_pct_wear_mask_all_time_weighted']]
+    print('NaNs before update:', df_to_update['smoothed_pct_wear_mask_all_time_weighted'].isna().sum())
+    df_to_update['smoothed_pct_wear_mask_all_time_weighted'].update(df_to_update['smoothedpct_wear_mask_all_time_weighted'])
+    print('NaNs after update:', df_to_update['smoothed_pct_wear_mask_all_time_weighted'].isna().sum())
+    # drop these two columns from the df regions
+    df.drop(columns='smoothedpct_wear_mask_all_time_weighted', axis=1, inplace=True)
+    # add the 'smoothed_pct_wear_mask_all_time_weighted' column from df_to_update to the regions set
+    df['smoothed_pct_wear_mask_all_time_weighted'] = df_to_update['smoothed_pct_wear_mask_all_time_weighted']
+    print('Updated.')
+    df = df[df['smoothed_pct_cli'].notna()]
+    return df
+
+    # Function to create a new dataframe for data on masks.
+def create_df_masks(df):
+    '''Function to create a data frame for data on masks.'''
+    df_masks = df[["country_agg","GID_0","region_agg","GID_1","country_region_numeric","gender","age_bucket","smoothed_pct_cli","date","month"]]
+    mask_names = df.columns[(df.columns.str.contains("mask") & (df.columns.str.contains("weighted")))]
+
+    for i in mask_names:
+        df_masks[str(i)] = df[str(i)]
+    
+    return df_masks
+
+    #Function to create two dictionaries based on the Human Development Index and its levels.
+def get_hdi(path,filename):
+    '''This function creates two dictionaries based on the Human Development Index and its levels.'''
+    
+    # Read in the xlsx-file with data
+    file = "{}/{}".format(path,filename)
+    hdi_data = pd.read_excel(file, header=None)
+    
+    # Replace the country names in the hdi_data with the corresponding country names as used in
+    # the regions data frame.
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Viet Nam", value ="Vietnam") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Czechia", value ="Czech Republic") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Russian Federation", value ="Russia") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Venezuela (Bolivarian Republic of)", value ="Venezuela") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Korea (Republic of)", value ="South Korea") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Bolivia (Plurinational State of)", value ="Bolivia") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Hong Kong, China (SAR)", value ="Hong Kong") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Moldova (Republic of)", value ="Moldova") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Tanzania (United Republic of)", value ="Tanzania") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Lao People's Democratic Republic", value ="Laos") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="Congo (Democratic Republic of the)", value ="Democratic Republic of the Congo") 
+    hdi_data[1] = hdi_data[1].replace(to_replace ="American Samoa", value ="Samoa")
+    
+    # Select the useful rows and columns from the hdi data file to make the hdi.
+    df_hdi = hdi_data.iloc[7:200, 1:3]
+    # Remove the title rows indicating the human development level.
+    df_hdi = df_hdi[df_hdi[2].notna()]
+    # Make a dictionary with countries as keys and the hdi as values.
+    dict_hdi = dict(df_hdi.values.tolist())
+    
+    # Select the useful rows and column from the hdi data file to make the hdi-levels.
+    df_levels = hdi_data.iloc[7:200, [1]]
+    # Create an index based on the title rows indicating the hdi-level.
+    idx = df_levels[(df_levels[1].str.contains("HUMAN DEVELOPMENT"))].index
+    # Use the index to create new dataframes per hdi-level.
+    df_very_high = df_levels.iloc[idx[0]-6:idx[1]-7, :]
+    df_high = df_levels.iloc[idx[1]-6:idx[2]-7, :]
+    df_medium = df_levels.iloc[idx[2]-6:idx[3]-7, :]
+    df_low = df_levels.iloc[idx[3]-6:, :]
+    # Add a column with the hdi-level per data frame.
+    df_very_high[2] = "very high"
+    df_high[2] = "high"
+    df_medium[2] = "medium"
+    df_low[2] = "low"
+    # Concatenate dataframes.
+    df_hdi_levels = pd.concat([df_very_high, df_high, df_medium, df_low])
+    # Make a dictionary with countries as keys and hdi-levels as values.
+    dict_hdi_levels = dict(df_hdi_levels.values.tolist())
+    
+    print('Creating dictionaries for hdi and hdi-levels completed.')
+    return dict_hdi, dict_hdi_levels
+
+    #Function creates a column in the regions table with the hdi and the hdi-levels.
+def create_hdi_columns(regions, dict_hdi, dict_hdi_levels):
+    '''This function uses the hdi dictionary and the hdi-levels dictionary to create columns in the regions table with the hdi and the hdi-levels.'''
+    
+    # Create empty lists for hdi and hdi levels
+    index = []
+    levels = []
+    
+    # Get the hdi values for the countries in the regions dataframe and append it to the list.
+    for i in range(len(regions)):  
+        hd_idx = dict_hdi.get(regions.at[regions.index[i],"country_agg"],"NaN")
+        index.append(hd_idx)
+    print('Creating hdi list completed.')
+    
+    # Get the hdi index for the countries in the regions dataframe and append it to the list.
+    for i in range(len(regions)):  
+        hdi_lvls = dict_hdi_levels.get(regions.at[regions.index[i],"country_agg"],"NaN")
+        levels.append(hdi_lvls)
+    print('Creating hdi-level list completed.')
+    
+    # Fill the appended lists into their corresponding columns.
+    regions["hdi"] = index
+    regions["hdi_level"] = levels
+    
+    return regions
---
+++ get_group.py
@@ -0,0 +1,85 @@
+# File with functions to generate various sub dataframes
+# Returns a list of dataframes
+
+def get_asia(df):
+    asia_w_names = ['Armenia', 'Azerbaijan', 'Israel', 'Jordan', 'Kuwait', 'Lebanon', 'Oman', 'Iraq', 
+                    'Qatar', 'Saudi Arabia', 'United Arab Emirates', 'Yemen']
+    asia_c_names = ['Kazakhstan', 'Kyrgyzstan', 'Uzbekistan']
+    asia_s_names = ['Bangladesh','India', 'Nepal', 'Afghanistan', 'Pakistan', 'Sri Lanka']
+    asia_e_names = ['Hong Kong', 'Japan', 'South Korea', 'Taiwan']
+    asia_se_names = ['Cambodia','Indonesia', 'Laos','Malaysia', 'Myanmar', 'Singapore', 'Thailand', 
+                    'Vietnam']
+
+    asia_w = df.loc[df["country_agg"].isin(asia_w_names)]
+    asia_c = df.loc[df["country_agg"].isin(asia_c_names)]
+    asia_s = df.loc[df["country_agg"].isin(asia_s_names)]
+    asia_e = df.loc[df["country_agg"].isin(asia_e_names)]
+    asia_se = df.loc[df["country_agg"].isin(asia_se_names)]
+
+    asia = [asia_w, asia_c, asia_s, asia_e, asia_se]
+    
+    return asia
+
+def get_oceania(df):
+    ceania_names = ["New Zealand", "Australia"]
+    oceania = countries.loc[countries["country_agg"].isin(oceania_names)] 
+
+    return oceania
+
+
+def get_europe(df):
+    europe_sc_names = ["Denmark", "Finland", "Norway", "Sweden"]
+    europe_w_names = ["Austria", "Belgium", "France", "Germany", "Ireland", "Netherlands", 
+                    "Switzerland", "United Kingdom"]
+    europe_s_names = ["Italy", "Portugal", "Spain", "Andorra"]
+    europe_se_names = ["Albania", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Greece", "Romania", 
+                        "Serbia", "Slovenia", "Turkey"]
+    europe_e_names = ["Belarus", "Czech Republic", "Hungary", "Moldova", "Poland", "Russia", 
+                        "Slovakia", "Ukraine"]
+    
+    europe_sc = df.loc[df["country_agg"].isin(europe_sc_names)]
+    europe_w = df.loc[df["country_agg"].isin(europe_w_names)]
+    europe_s = df.loc[df["country_agg"].isin(europe_s_names)]
+    europe_se = df.loc[df["country_agg"].isin(europe_se_names)]
+    europe_e = df.loc[df["country_agg"].isin(europe_e_names)]
+
+    europe = [europe_sc, europe_w, europe_s, europe_se, europe_e]
+
+    return europe
+
+
+def get_africa(df):
+    africa_e_names = ['Ethiopia', 'Kenya', 'Madagascar', 'Mozambique', 'Tanzania']
+    africa_m_names = ['Angola', 'Cameroon','Democratic Republic of the Congo']
+    africa_n_names = ['Algeria', 'Egypt', 'Libya','Morocco', 'Tunisia', 'Sudan']
+    africa_s_names = ['South Africa']
+    africa_w_names = ['Benin', 'Burkina Faso', "Côte d'Ivoire", 'Ghana', 'Guinea', 'Mali', 
+                    'Mauritania', 'Nigeria', 'Senegal']
+    
+    africa_e = df.loc[df["country_agg"].isin(africa_e_names)]
+    africa_m = df.loc[df["country_agg"].isin(africa_m_names)]
+    africa_n = df.loc[df["country_agg"].isin(africa_n_names)]
+    africa_s = df.loc[df["country_agg"].isin(africa_s_names)]
+    africa_w = df.loc[df["country_agg"].isin(africa_c_names)]
+
+    africa = [africa_e, africa_m, africa_n, africa_s, africa_w]
+
+    return africa
+
+
+def get_americas(df):
+    america_n_names = ["Canada"]
+    america_ce_names = ["Costa Rica", "El Salvador", "Guatemala", "Honduras", "Mexico", "Nicaragua", 
+                        "Panama"]
+    america_c_names = ["Dominican Republic", "Haiti"]
+    america_s_names = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Paraguay", 
+                        "Peru", "Uruguay", "Venezuela"]
+    
+    america_n = df.loc[df["country_agg"].isin(america_n_names)]
+    america_ce = df.loc[df["country_agg"].isin(america_ce_names)]
+    america_c = df.loc[df["country_agg"].isin(america_c_names)]
+    america_s = df.loc[df["country_agg"].isin(america_s_names)]
+
+    america = [america_n, america_ce, america_c, america_s]
+
+    return america
