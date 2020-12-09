db6c080100e59f6f225e67bbb27d0850279deab0
Laura Dell'Antonio <71877418+lauradellantonio@users.noreply.github.com>
2020-12-09 12:47:45 +0100

    Completed Dashboard (#49)
    * Updated function (#48)
    
    * Notebook and function for joining mask wearing requirements with survey data. (#45)
    
    Co-authored-by: Jakolien den Hollander <jakoliendenhollander@Jakoliens-MBP.fritz.box>
    
    * Updated corona_stats to set negatives to 0 and added status printouts to corona_stats and mask_req. (#47)
    
    Co-authored-by: Jakolien den Hollander <jakoliendenhollander@Jakoliens-MBP.fritz.box>
    
    Co-authored-by: Jakolien den Hollander <72009367+jakoliendenhollander@users.noreply.github.com>
    Co-authored-by: Jakolien den Hollander <jakoliendenhollander@Jakoliens-MBP.fritz.box>
    
    * added time feature to app
    
    * Adjusted sizing
    
    * Added more drop down options to dashboard and made notebook for old versions of app.py
    
    * Added all social distancing measures
    
    * added color ranges
    
    * full data in csv file
    
    * completed (not super pretty)
    
    * tiding
    
    Co-authored-by: Jakolien den Hollander <72009367+jakoliendenhollander@users.noreply.github.com>
    Co-authored-by: Jakolien den Hollander <jakoliendenhollander@Jakoliens-MBP.fritz.box>

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ df = pd.read_csv("dash_data/countries_dash.csv.gzip", compression="gzip")
 #App layout
 app.layout = html.Div([
     
-    html.H1("Dashboard for Capstone", style={"text-align": "centre"}),
+    html.H1("Corona Pandemic over Time", style={"text-align": "centre"}),
     
     dcc.Dropdown(id="slct_gender",
                 options=[
@@ -41,7 +41,23 @@ app.layout = html.Div([
                     {"label": "Total cases per million", "value": "total_cases_per_million"},
                     {"label": "Total deaths per million", "value": "total_deaths_per_million"},
                     {"label": "Median Age", "value": "median_age"},
-                    {"label": "Human Development Index", "value": "hdi"}],
+                    {"label": "Human Development Index", "value": "hdi"},
+                    {"label": "Total number of responses", "value": "rolling_total_responses"},
+                    {"label": "Percentage of people with COVID-like illness", "value": "smoothed_pct_cli_weighted"},
+                    {"label": "Percentage of people working outside their own home", "value": "smoothed_pct_worked_outside_home_weighted"},
+                    {"label": "Percentage of people leaving their house for groceries/pharmacy", "value": "smoothed_pct_grocery_outside_home_weighted"},
+                    {"label": "Percentage of people eating meals outside their own home", "value": "smoothed_pct_ate_outside_home_weighted"},
+                    {"label": "Percentage of people spending time with non-household members", "value": "smoothed_pct_spent_time_with_non_hh_weighted"},
+                    {"label": "Percentage of people attending public events", "value": "smoothed_pct_attended_public_event_weighted"},
+                    {"label": "Percentage of people using public transport", "value": "smoothed_pct_used_public_transit_weighted"},
+                    {"label": "Percentage of people in direct contact with non-household members", "value": "smoothed_pct_direct_contact_with_non_hh_weighted"},
+                    {"label": "Percentage of people who haven't been in public in 7 days", "value": "smoothed_pct_no_public_weighted"},
+                    {"label": "Percentage of people wearing masks all the time", "value": "smoothed_pct_wear_mask_all_time_weighted"},
+                    {"label": "Percentage of people wearing masks most of the time", "value": "smoothed_pct_wear_mask_most_time_weighted"},
+                    {"label": "Percentage of people wearing masks half of the time", "value": "smoothed_pct_wear_mask_half_time_weighted"},
+                    {"label": "Percentage of people wearing masks some times", "value": "smoothed_pct_wear_mask_some_time_weighted"},
+                    {"label": "Percentage of people never wearing masks", "value": "smoothed_pct_wear_mask_none_time_weighted"}],
+
                 multi=False,
                 value="total_cases_per_million",
                 style={"width": "40%"}
@@ -50,7 +66,7 @@ app.layout = html.Div([
     html.Div(id="output_container", children=[]),
     html.Br(),
     
-    dcc.Graph(id="my_covid_map", figure={})
+    dcc.Graph(id="my_covid_map", figure={}, style={"width": "150vh", "height": "90vh"})
     
 ])
 
@@ -69,16 +85,41 @@ def update_graph(ddgender, option):
     print(ddgender, option)
     #print(type(option_slct1, option_slct2))
     
-    container = "The user chose {} for the gender and to display {}".format(ddgender, option)
+    container = "The user chose to display {} for {} gender".format(option, ddgender)
     
     dff = df[(df["data_cat"]==option) & (df["gender"]==ddgender)]
     
+    ranges = {
+        "total_cases_per_million": [0, 63500],   
+        "total_deaths_per_million": [0, 1070],
+        "rolling_total_responses": [50,120000],
+        "smoothed_pct_cli_weighted": [0, 62],
+        "smoothed_pct_worked_outside_home_weighted": [0, 90],
+        "smoothed_pct_grocery_outside_home_weighted": [0, 93],
+        "smoothed_pct_ate_outside_home_weighted": [0, 86],
+        "smoothed_pct_spent_time_with_non_hh_weighted": [0, 83],
+        "smoothed_pct_attended_public_event_weighted": [0, 74],
+        "smoothed_pct_used_public_transit_weighted": [0, 80],
+        "smoothed_pct_direct_contact_with_non_hh_weighted": [0, 90],
+        "smoothed_pct_no_public_weighted": [0, 67],
+        "smoothed_pct_wear_mask_all_time_weighted": [0, 96],
+        "smoothed_pct_wear_mask_most_time_weighted": [0, 52],
+        "smoothed_pct_wear_mask_half_time_weighted": [0, 42],
+        "smoothed_pct_wear_mask_some_time_weighted": [0, 46],
+        "smoothed_pct_wear_mask_none_time_weighted": [0, 94]
+        }
+
+
     #Plotly Express
     fig = px.choropleth(
         data_frame = dff,
         locations="iso_code",
         color="amount",
+        range_color=ranges.get(option),
         hover_name="country_agg",
+        hover_data=["date", "data_cat", "amount"],
+        animation_frame = "date",
+        projection = "natural earth",
         color_continuous_scale=["white", "#00c5ff", "#00287f", "#00151f"]
     )
     
